831af569b3fc6265edd6ed1eb50b63705334a5aa
Louis Lohmer <Louis.Lohmer@mediaman.de>
2024-12-23 10:18:06 +0100

    create and validate form to edit a player

--- teamverwaltungssoftware/src/components/settings/EditPlayer.vue
+++ teamverwaltungssoftware/src/components/settings/EditPlayer.vue
@@ -1,22 +1,112 @@
 <template>
-    <h1>test</h1>
-    <div class="buttons">
-        <input v-model="PlayerName" placeholder="PlayerName" />
-    </div>
+    <h1>Edit existing player</h1>
+    <form class="edit-player-form">
+        <label for="playername">Playername:</label>
+        <input type="text" id="playername" name="playername" />
+        <label for="firstname">firstname:</label>
+        <input type="text" id="firstname" name="firstname" />
+        <label for="lastname">lastname:</label>
+        <input type="text" id="lastname" name="lastname" />
+        <label for="email">E-Mail:</label>
+        <input type="email" id="email" name="email" />
+        <label for="position">Position:</label>
+        <input type="text" id="position" name="position" />
+        <label for="eloPoints">Elo-Points:</label>
+        <input type="number" min="0" id="eloPoints" name="eloPoints" />
+        <button type="button" @click="editPlayer">Edit player</button>
+    </form>
+    <span class="error-message">{{ textErrorMessage }}</span>
+    <button type="button" @click="deletePlayer">Delete player</button>
     <RouterLink to="/PlayerSettings"> Back </RouterLink>
 </template>
 
 <script>
 export default {
-    name: 'EditPlayer',
+    data() {
+        return {
+            name: 'EditPlayer',
+            textErrorMessage: '',
+        };
+    },
+    methods: {
+        deletePlayer() {
+            console.log('delete player');
+        },
+        editPlayer() {
+            console.log('edit player');
+            let playername = document.getElementById('playername').value;
+            let firstname = document.getElementById('firstname').value;
+            let lastname = document.getElementById('lastname').value;
+            let email = document.getElementById('email').value;
+            let position = document.getElementById('position').value;
+            let eloPoints = document.getElementById('eloPoints').value;
+
+            if (
+                playername.length === 0 &&
+                firstname.length === 0 &&
+                lastname.length === 0 &&
+                email.length === 0 &&
+                position.length === 0 &&
+                eloPoints.length === 0
+            ) {
+                this.textErrorMessage = 'Please fill out at least one form field!';
+                return;
+            } else {
+                this.textErrorMessage = '';
+            }
+
+            if (firstname.length > 0) {
+                let splittedFirstname = firstname.split('');
+                const onlyLettersRegEx = /^[a-zA-Z]+$/;
+
+                for (let i = 0; i < splittedFirstname.length; i++) {
+                    if (!splittedFirstname[i].match(onlyLettersRegEx)) {
+                        this.textErrorMessage = 'Invalid firstname!';
+                        return;
+                    }
+                }
+            } else {
+                this.textErrorMessage = '';
+            }
+
+            if (lastname.length > 0) {
+                let splittedLastname = lastname.split('');
+                const onlyLettersRegEx = /^[a-zA-Z]+$/;
+
+                for (let i = 0; i < splittedLastname.length; i++) {
+                    if (!splittedLastname[i].match(onlyLettersRegEx)) {
+                        this.textErrorMessage = 'Invalid lastname!';
+                        return;
+                    }
+                }
+            } else {
+                this.textErrorMessage = '';
+            }
+
+            if (email.length > 0) {
+                const regExEmail = /^[a-z0-9.]+@[a-z]+\.[a-z]{2,4}$/;
+                if (!email.match(regExEmail)) {
+                    this.textErrorMessage = 'Invalid E-mail!';
+                    return;
+                }
+            } else {
+                this.textErrorMessage = '';
+            }
+        },
+    },
 };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-.buttons {
+.edit-player-form {
     display: flex;
     justify-content: center;
     flex-direction: column;
+}
+
+.error-message {
+    color: red;
+    font-size: 20px;
 }
 </style>
