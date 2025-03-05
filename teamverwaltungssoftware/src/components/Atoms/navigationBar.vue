1ccec5fdab4511fa35e6e02f42b81f6120c25600
Louis Lohmer <Louis.Lohmer@mediaman.de>
2025-03-05 16:22:11 +0100

    change favicon to company logo and heading to header

--- teamverwaltungssoftware/src/components/Atoms/navigationBar.vue
+++ teamverwaltungssoftware/src/components/Atoms/navigationBar.vue
@@ -1,34 +1,52 @@
 <template>
-    <ul>
-        <!--WARNING: Do not remove id of list elements, they are used to indicate the current visited page-->
-        <li><RouterLink to="/" id="homeComponent-routerlink">Home</RouterLink></li>
-        <!-- <li><RouterLink to="/OneVsOne">1 vs 1</RouterLink></li>
+    <header>
+        <nav>
+            <ul>
+                <!--WARNING: Do not remove id of list elements, they are used to indicate the current visited page-->
+                <li><RouterLink to="/" id="homeComponent-routerlink">Home</RouterLink></li>
+                <!-- <li><RouterLink to="/OneVsOne">1 vs 1</RouterLink></li>
         <li><RouterLink to="/TeamVsTeam">T vs T</RouterLink></li>
         <li><RouterLink to="/TeamTournament">Tournament</RouterLink></li>
         <li><RouterLink to="/SettingScreen">Settings</RouterLink></li> -->
-        <li><RouterLink to="/Leaderboards" id="leaderboard-routerlink">Leaderboards</RouterLink></li>
-    </ul>
+                <li><RouterLink to="/Leaderboards" id="leaderboard-routerlink">Leaderboard</RouterLink></li>
+            </ul>
+            <h1>Teamverwaltungssoftware</h1>
+        </nav>
+    </header>
 </template>
 
 <style scoped>
+h1 {
+    font-size: 23px;
+    color: var(--white);
+    padding-right: 20px;
+    margin: 0;
+}
+
 ul {
     list-style-type: none;
     margin: 0;
     padding: 0;
-    overflow: hidden;
-    background-color: var(--black);
 }
 
-ul {
+header {
     position: fixed;
     top: 0;
     width: 100%;
     left: 0;
     right: 0;
+    background-color: var(--black);
+}
+
+nav {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
 }
 
 li {
     float: left;
+    border-right: 2px solid var(--white);
 }
 
 li a {
@@ -43,10 +61,6 @@ li a:hover {
     background-color: var(--hovergreen);
 }
 
-li {
-    border-right: 2px solid var(--lightgrey);
-}
-
 /* Responsive Anpassungen für Tablets*/
 /* @media only screen and (min-width: 768px) and (max-width: 1023px) {
 } */
@@ -55,7 +69,16 @@ li {
 @media only screen and (max-width: 767px) {
     li a {
         font-size: 14px;
-        padding: 5px 5px;
+        padding: 3px 3px;
+    }
+
+    li {
+        width: 90%;
+    }
+
+    h1 {
+        font-size: 19px;
+        padding-right: 5px;
     }
 }
 </style>
